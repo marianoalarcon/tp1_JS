225427f9be289077ffe7a2142b07e02c6c27712e
Mariano Alarcon <mariano.alarcon@hotmail.com>
2017-08-20 04:37:36 -0300

    Crear funcion
    Crear funcion aplicando el metodo prompt e implementando button

--- js/investigacion.js
+++ js/investigacion.js
@@ -1,3 +1,8 @@
 //alert("Hola");
-var nombre = prompt("ingresa tu nombre", "pepito");
-document.write("<p> Has ingresado correctamente tu nombre de usuario es "+"<span id='color'>" + nombre +"</span </p>");
+function my_function() {
+	var nombre = prompt("ingresa tu nombre", "pepito");
+	if (nombre != null) {
+		document.write("<p> Has ingresado correctamente tu nombre de usuario es "+"<span id='color'>" + nombre +"</span </p>");
+	}
+}
+
--- js/punto4.js
+++ js/punto4.js
@@ -1,4 +1,4 @@
-var a = true;
+var a = true; //Declaracion de variables
 var b = false;
 document.write ("<th class='color1'> " + "p" +"</th>"+ " <th class='color1'>" + "q" + "</th> " + " <th class='color1'>" + "Operador AND (p && q)" + " </th>");
 document.write ("<tr class='color'> <td>" + a +"</td>"+ " <td>" + a + "</td> " + " <td>" + (a&&a) + "</td></tr>");
